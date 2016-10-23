05a957a664d6fc4f7886ca969f245d6ed9610f44
Kamal Kumar <kamalkumar@Kamals-MacBook-Pro.local>
2016-10-23 15:24:46 -0400

    done, just adding comments

--- src/app.js
+++ src/app.js
@@ -1,7 +1,32 @@
-function openModal() {
-  document.getElementById('myModal').style.display = "block";
-}
 
-function closeModal() {
-  document.getElementById('myModal').style.display = "none";
-}
+//checking is javascript is working
+console.log("hello everybody")
+
+//calling on the entire html file
+// var all = document.getElementById("all");
+//console logging to make sure it is, what it is
+// console.log(all)
+
+//test cses
+// var main = document.getElementById("main")
+// console.log(main)
+
+//calling the div file with everything in it
+var master = document.getElementById("master")
+//console logging to make sure it is
+console.log(master)
+
+//getting the x button
+var btn = document.getElementById("x")
+//console logging to make sure it is what it is
+console.log(btn)
+
+//when user clicks the x, the event listener enables the function
+btn.addEventListener("click",function(){
+	//console logging to see where i am in the function
+	console.log("event listener is working")
+	//calling the element from line 15 and then making it empty
+	master.innerHTML = "";
+	//console logging to make sure its working
+	console.log("it worked")
+})
